28dde5bbb34cf20fe1db1e45650227ef94c386d6
Eka Fajhari Adwar <eka.adwar@gmail.com>
2020-01-29 06:58:26 +0700

    mengatur tata letak tabel pendaftaran pada file or_daftar.php

--- page/or_daftar.php
+++ page/or_daftar.php
@@ -1,70 +1,72 @@
 <form>
 	<table>
 		<tr>
-			<td colspan="3">
+			<th colspan="3">
 				Formulir Pendaftaran Open Recruitment
-			</td>
+			</th>
 		</tr>
 		<tr>
-			<td>Nama</td>
+			<td class="judul">Nama</td>
 			<td>:</td>
 			<td><input type="text" name="nama"></td>
 		</tr>
 		<tr>
-			<td>No.Bp</td>
+			<td class="judul">No.Bp</td>
 			<td>:</td>
 			<td><input type="text" name="nobp"></td>
 		</tr>
 		<tr>
-			<td>E-mail</td>
+			<td class="judul">E-mail</td>
 			<td>:</td>
 			<td><input type="text" name="email"></td>
 		</tr>
 		<tr>
-			<td>Fakultas</td>
+			<td class="judul">Fakultas</td>
 			<td>:</td>
 			<td><input type="text" name="fakultas"></td>
 		</tr>
 		<tr>
-			<td>Jurusan</td>
+			<td class="judul">Jurusan</td>
 			<td>:</td>
 			<td><input type="text" name="jurusan"></td>
 		</tr>
 		<tr>
-			<td>Tanggal Lahir</td>
+			<td class="judul">Tanggal Lahir</td>
 			<td>:</td>
 			<td><input type="text" name="tgl_lahir"></td>
 		</tr>
 		<tr>
-			<td>Tempat Lahir</td>
+			<td class="judul">Tempat Lahir</td>
 			<td>:</td>
 			<td><input type="text" name="tmp_lahir"></td>
 		</tr>
 		<tr>
-			<td>Alamat Asal</td>
+			<td class="judul">Alamat Asal</td>
 			<td>:</td>
 			<td><input type="text" name="alamat"></td>
 		</tr>
 		<tr>
-			<td>Bidang Minat</td>
+			<td class="judul">Bidang Minat</td>
 			<td>:</td>
 			<td><input type="text" name="bidang"></td>
 		</tr>
 		<tr>
-			<td>Alasan Masuk KMTZ</td>
+			<td class="judul">Alasan Masuk KMTZ</td>
 			<td>:</td>
 			<td><input type="text" name="alasan"></td>
 		</tr>
 		<tr>
-			<td>Foto</td>
+			<td class="judul">Foto</td>
 			<td>:</td>
 			<td><input type="text" name="foto"></td>
 		</tr>
+		<div class="clear"></div>
 		<tr>
 			<td colspan="3">
-				<input type="submit" name="daftar" value="Daftar">
-				<input type="reset" name="reset" value="Batal">
+				<input class="tombol" type="submit" name="daftar" value="Daftar">
+				<input class="tombol" type="reset" name="reset" value="Batal">
 			</td>
 		</tr>
+		<div class="clear"></div>
 	</table>
 </form>
