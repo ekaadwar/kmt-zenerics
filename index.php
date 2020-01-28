75ecec579e0a92a7740528173bedcedb5d389790
Eka Fajhari Adwar <eka.adwar@gmail.com>
2020-01-29 06:11:01 +0700

    memindahkan isi pada file or_daftar.php ke bagian isi pada file index.php

--- index.php
+++ index.php
@@ -24,7 +24,71 @@
 			</ul>
 		</div>
 		<div class="isi">
-			ini adalah bagian isi
+			<form>
+				<table>
+					<tr>
+						<td>Nama</td>
+						<td>:</td>
+						<td><input type="text" name="nama"></td>
+					</tr>
+					<tr>
+						<td>No.Bp</td>
+						<td>:</td>
+						<td><input type="text" name="nobp"></td>
+					</tr>
+					<tr>
+						<td>E-mail</td>
+						<td>:</td>
+						<td><input type="text" name="email"></td>
+					</tr>
+					<tr>
+						<td>Fakultas</td>
+						<td>:</td>
+						<td><input type="text" name="fakultas"></td>
+					</tr>
+					<tr>
+						<td>Jurusan</td>
+						<td>:</td>
+						<td><input type="text" name="jurusan"></td>
+					</tr>
+					<tr>
+						<td>Tanggal Lahir</td>
+						<td>:</td>
+						<td><input type="text" name="tgl_lahir"></td>
+					</tr>
+					<tr>
+						<td>Tempat Lahir</td>
+						<td>:</td>
+						<td><input type="text" name="tmp_lahir"></td>
+					</tr>
+					<tr>
+						<td>Alamat Asal</td>
+						<td>:</td>
+						<td><input type="text" name="alamat"></td>
+					</tr>
+					<tr>
+						<td>Bidang Minat</td>
+						<td>:</td>
+						<td><input type="text" name="bidang"></td>
+					</tr>
+					<tr>
+						<td>Alasan Masuk KMTZ</td>
+						<td>:</td>
+						<td><input type="text" name="alasan"></td>
+					</tr>
+					<tr>
+						<td>Foto</td>
+						<td>:</td>
+						<td><input type="text" name="foto"></td>
+					</tr>
+					<tr>
+						<td colspan="3">
+							<input type="submit" name="daftar" value="Daftar">
+							<input type="reset" name="reset" value="Batal">
+						</td>
+					</tr>
+				</table>
+			</form>
 		</div>
 		<div class="footer">
 			<table>
