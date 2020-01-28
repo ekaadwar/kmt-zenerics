9a0fd441721b08e65ac0f9e87c28535b4ab5f022
Eka Fajhari Adwar <eka.adwar@gmail.com>
2020-01-28 16:21:54 +0700

    membuat file index.php dan style.css

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>KMT Zenerics</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="canvas">
+		<div class="header">
+			<img src="desain/header.png" width="1200px">
+		</div>
+		<div class="namebar">
+			<p style="margin:0;">Selamat datang, Pak Eka</p>
+		</div>
+		<div class="menu">
+			<ul>
+				<li>BERITA</li>
+				<li>STRUKTUR ORGANISASI</li>
+				<li>OPEN RECRUITMENT</li>
+				<li>PENGUMUMAN</li>
+				<li>PROFIL</li>
+				<li>LOG OUT</li>
+			</ul>
+		</div>
+		<div class="isi">
+			ini adalah bagian isi
+		</div>
+		<div class="footer">
+			
+		</div>
+	</div>
+</body>
+</html>
