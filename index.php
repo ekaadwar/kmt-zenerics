3dfcbe9f6fcf6c313772462e4bcc18f94af0d425
Eka Fajhari Adwar <eka.adwar@gmail.com>
2020-01-28 17:06:41 +0700

    membuat desain tampilan web[F

--- index.php
+++ index.php
@@ -10,23 +10,37 @@
 			<img src="desain/header.png" width="1200px">
 		</div>
 		<div class="namebar">
-			<p style="margin:0;">Selamat datang, Pak Eka</p>
+			<a href="">Selamat datang, Pak Eka</a>
 		</div>
 		<div class="menu">
 			<ul>
-				<li>BERITA</li>
-				<li>STRUKTUR ORGANISASI</li>
-				<li>OPEN RECRUITMENT</li>
-				<li>PENGUMUMAN</li>
-				<li>PROFIL</li>
-				<li>LOG OUT</li>
+				<li><a href="">BERITA</a></li>
+				<li><a href="">STRUKTUR ORGANISASI</a></li>
+				<li><a href="">OPEN RECRUITMENT</a></li>
+				<li><a href="">PENGUMUMAN</a></li>
+				<li class="kanan"><a href="">LOG OUT</a></li>
+				<li class="kanan"><a href="">PROFIL</a></li>
+				<div class="clear"></div>
 			</ul>
 		</div>
 		<div class="isi">
 			ini adalah bagian isi
 		</div>
 		<div class="footer">
-			
+			<table>
+				<tr>
+					<td>Alamat</td>
+					<td>.</td>
+				</tr>
+				<tr>
+					<td><a href="">Facebook</a></td>
+					<td class="maker"><a href="">created by : Eka Fajhari Adwar</a></td>
+				</tr>
+				<tr>
+					<td><a href="">Instagram</a></td>
+					<td class="maker">ZNS.002/GNR</td>
+				</tr>
+			</table>
 		</div>
 	</div>
 </body>
