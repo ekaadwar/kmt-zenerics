dfeec62370b9fd48d87a075f460d13ecbd86e9cc
Eka Fajhari Adwar <eka.adwar@gmail.com>
2020-01-29 18:00:20 +0700

    sedang membuat sistem menu menggunakan fungsi GET

--- section/isi.php
+++ section/isi.php
@@ -1,5 +1,9 @@
 <div class="isi">
 	<?php 
+		$page 	= $_GET['page'];
+		$action = $_GET['action'];
+		
+		if($page = )
 		include "page/or_daftar.php";
 	 ?>
 </div>
--- section/menu.php
+++ section/menu.php
@@ -1,11 +1,11 @@
 <div class="menu">
 	<ul>
-		<li><a href="">BERITA</a></li>
-		<li><a href="">STRUKTUR ORGANISASI</a></li>
-		<li><a href="">OPEN RECRUITMENT</a></li>
-		<li><a href="">PENGUMUMAN</a></li>
-		<li class="kanan"><a href="">LOG OUT</a></li>
-		<li class="kanan"><a href="">PROFIL</a></li>
+		<li><a href="?page=berita">BERITA</a></li>
+		<li><a href="?page=stuktur">STRUKTUR ORGANISASI</a></li>
+		<li><a href="?page=or">OPEN RECRUITMENT</a></li>
+		<li><a href="?page=pengumuman">PENGUMUMAN</a></li>
+		<li class="kanan"><a href="page/logout.php">LOG OUT</a></li>
+		<li class="kanan"><a href="?page=profil">PROFIL</a></li>
 		<div class="clear"></div>
 	</ul>
 </div>
