e167ade4ee3b86efae5b2d800818d0bf9f20b171
Eka Fajhari Adwar <eka.adwar@gmail.com>
2020-01-30 14:12:44 +0700

    membuat tabel  di dalam database  melalui file

---
+++ mysql/tb_pesertaOr.php
@@ -0,0 +1,41 @@
+<?php 
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "kmt";
+
+	// create connection
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+	// check connection
+	if(!$conn){
+		die("Connection failed : ". mysqli_connect_error());
+	}
+
+	// sql to create table
+	$sql = "CREATE TABLE `pesertaOr` ( `index` INT(4) NOT NULL AUTO_INCREMENT ,
+										 `nama` VARCHAR(50) NOT NULL , 
+										 `nobp` VARCHAR(50) NOT NULL , 
+										 `email` VARCHAR(50) NOT NULL , 
+										 `fakultas` VARCHAR(100) NOT NULL , 
+										 `jurusan` VARCHAR(100) NOT NULL , 
+										 `tgl_lahir` VARCHAR(50) NOT NULL , 
+										 `tmp_lahir` VARCHAR(50) NOT NULL , 
+										 `gender` ENUM('Laki-laki','Perempuan') NOT NULL , 
+										 `alamat` TEXT NOT NULL , 
+										 `komputer` INT(1) NOT NULL , 
+										 `robotika` INT(1) NOT NULL , 
+										 `alasan` TEXT NOT NULL , 
+										 `username` VARCHAR(50) NOT NULL , 
+										 `password` VARCHAR(50) NOT NULL , 
+										 `time` TIMESTAMP NOT NULL , 
+										 PRIMARY KEY (`index`)
+									) ENGINE = InnoDB;";
+	if(mysqli_query($conn, $sql)){
+		echo "Table `pesertaOr` created successfully";
+	}else{
+		echo "Error creating table : ". mysqli_error($conn);
+	}
+
+	mysqli_close($conn);
+ ?>
