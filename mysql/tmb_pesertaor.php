d63a2259ccfaa36628d0aa88a0df110ca12a8928
Eka Fajhari Adwar <eka.adwar@gmail.com>
2020-02-03 07:49:30 +0700

    menambahkan kolom 'username' dan 'password' pada tabel 'pesertaor' melalui file 'tmb_pesertaor.php'

---
+++ mysql/tmb_pesertaor.php
@@ -0,0 +1,17 @@
+<?php 
+	#--------------------------------------------------------
+	#program untuk menambahkan kolom 'username' dan 'password' pada tabel pesertaor.php
+	#--------------------------------------------------------
+	include("koneksi.php");
+
+	$sql = "ALTER TABLE `pesertaor` ADD `username` VARCHAR(100) NOT NULL AFTER `alasan`, ADD `password` VARCHAR(100) NOT NULL AFTER `username`;";
+
+	$result = mysqli_query($conn, $sql);
+
+	if($result){
+		echo "Kolom username dan password telah ditambahkan.";
+	}else{
+		echo "Penambahan kolom gagal.";
+	}
+
+ ?>
